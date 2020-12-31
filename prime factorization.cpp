ebbcea3742d449ba39a9394ea885f7b8218aab5d
iteachmachines <iteachmachines@gmail.com>
2020-12-31 15:17:32 +0530

    Prime factorization

---
+++ prime factorization.cpp
@@ -0,0 +1,29 @@
+//prime factorization
+#include<bits/stdc++.h>
+using namespace std;
+
+void primefactor(int n)
+{
+	while(n%2==0)
+	{
+		cout<<2<<" ";
+		n/=2;
+	}
+	for(int i=3;i<=sqrt(n);i=i+2)
+	{
+		while(n%i==0)
+		{
+			cout<<i<<" ";
+			n=n/i;
+		}
+	}
+	if(n>2)
+    cout<<n<<" ";
+}
+int main()
+{
+	int n;
+	cin>>n;
+	primefactor(n);
+	return 0;
+}
